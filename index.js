09e01521feaf7eb9947868c54a0f6d26b3d7e7ee
Stefan Stefanov <s.h.stefanov@mail.bg>
2016-02-10 17:53:15 +0200

    wip

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+require("reflect-metadata");
+var ng = require("angular2");
+
+
 var Controller = require("infrastructure/lib/client/Controller");
+var helpers = require("infrastructure/lib/helpers");
+
+// init(options: Object, cb: Function){
+//   if(!document.body){ window.onload = () =>  this.init(options, cb); return; }
+
+//   helpers.chain([
+//     (cb: Function) => {
+//       console.log("Chain 1", this);
+//       cb();
+//     },
+
+//     (cb: Function) => {
+//       // MyClassDebug
+//       @Component({
+//         selector: "my-selector",
+//         template: "<h1>My template</h1>"
+//       });
+//       class MyClass{
+//         initialize(){
+//           console.log("something")
+//         }
+//       }
+
+//     }
+
+
+//   ])(cb);
+
+// }
+
 
 module.exports = Controller.extend("Anfular2AppController", {
   init: function(options, cb){
-    console.log("INIT ::: ");
-    cb();
+     helpers.chain([
+      function(cb){
+        if(this.Layout) ng.platform.browser.bootstrap(this.Layout);
+        cb();
+      }
+
+
+    ])(cb, this);
   }
 });
