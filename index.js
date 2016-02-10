51a62e61e442c8d741de201fe4e9e774bd08984a
Stefan Stefanov <s.h.stefanov@mail.bg>
2016-02-10 12:32:25 +0200

    Adding more dependencies

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Controller = require("infrastructure/lib/client/Controller");
 
 module.exports = Controller.extend("Anfular2AppController", {
   init: function(options, cb){
-    
+    console.log("INIT ::: ");
+    cb();
   }
 });
